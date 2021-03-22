27368a9df856df9e479813c516023efd5f141c42
Victor Waichigo <v-viwaic@microsoft.com>
2021-03-22 06:54:22 +0300

    Add new app routes to the app.py

--- app.py
+++ app.py
@@ -5,7 +5,35 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-   return render_template("index.html")
+   title = "Beyond Dothan Experience"
+   return render_template("index.html", title=title)
+    #  return "Hello Flask"
+
+
+@app.route("/who_we_are")
+def who_we_are():
+   return render_template("who_we_are.html")
+    #  return "Hello Flask"
+
+
+
+@app.route("/learn_with_us")
+def learn_with_us():
+   return render_template("learn_with_us.html")
+    #  return "Hello Flask"
+
+
+
+@app.route("/connect_with_us")
+def connect_with_us():
+   return render_template("connect_with_us.html")
+    #  return "Hello Flask"
+
+
+
+@app.route("/our_resources")
+def our_resources():
+   return render_template("our_resources.html")
     #  return "Hello Flask"
 
 
